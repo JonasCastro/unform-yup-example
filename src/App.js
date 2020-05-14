51d636242a4401aae0717a3353b37344694702f9
Jonas Castro <jonascastro128@gmail.com>
2020-05-14 11:34:51 -0300

    lang="en"

--- src/App.js
+++ src/App.js
@@ -1,68 +1,67 @@
-import React, { useRef, useEffect} from 'react';
-import { Form } from '@unform/web'
-import { Scope } from '@unform/core'
-import './App.css';
-import * as Yup from 'yup';
+import React, { useRef, useEffect } from "react";
+import { Form } from "@unform/web";
+import { Scope } from "@unform/core";
+import "./App.css";
+import * as Yup from "yup";
 
-import Input from './components/Form/Input'
+import Input from "./components/Form/Input";
 
 function App() {
   const formRef = useRef(null);
 
-  async function handleSubmit(data,{reset}){
+  async function handleSubmit(data, { reset }) {
     try {
       const schema = Yup.object().shape({
-        name: Yup.string().required('O nome é obrigatório'),
+        name: Yup.string().required("Name is required."),
         email: Yup.string()
-        .email('Digite um e-mail válido.')
-        .required('O e-mail é obrigatório.'),
+          .email("Enter a valid email address.")
+          .required("Email is required."),
         address: Yup.object().shape({
-          city: Yup.string().min(3, 'No mínimo 3 caracteres').required('A cidade é obrigatório.')
-        })
+          city: Yup.string()
+            .min(3, "At least 3 characters")
+            .required("City is required."),
+        }),
       });
 
-      await schema.validate(data,{
+      await schema.validate(data, {
         abortEarly: false,
-      })
-      alert(JSON.stringify(data))
+      });
+      alert(JSON.stringify(data));
       formRef.current.setErrors({});
       reset();
     } catch (err) {
-      if(err instanceof Yup.ValidationError){
+      if (err instanceof Yup.ValidationError) {
         const errorMessages = {};
-        err.inner.forEach(error =>{
+        err.inner.forEach((error) => {
           errorMessages[error.path] = error.message;
-        })
+        });
         formRef.current.setErrors(errorMessages);
       }
     }
-
   }
 
-  useEffect(()=>{
-    setTimeout(()=>{
+  useEffect(() => {
+    setTimeout(() => {
       formRef.current.setData({
-        address:{
-          state:'Ce'
-        }
-      })
-    },200);
-  },[]);
+        address: {
+          state: "Ce",
+        },
+      });
+    }, 200);
+  }, []);
 
   return (
     <div className="App">
       <div className="content">
         <section>
-          <h1>Exemplo de uso Unform e Yup</h1>
-
-          </section>
-        <Form ref={formRef}  onSubmit={handleSubmit}>
-          <Input name="name"  label="Nome*" />
-          <Input type="email" name="email" label="E-mail*"/>
+          <h1>Example usage Unform and Yup</h1>
+        </section>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input name="name" label="Name*" />
+          <Input type="text" name="email" label="E-mail*" />
 
           <Scope path="address">
-            <Input name="city" label="Cidade*"/>
-            <Input name="state" label="Estado*"/>
+            <Input name="city" label="City*" />
           </Scope>
 
           <button type="submit">Enviar</button>
